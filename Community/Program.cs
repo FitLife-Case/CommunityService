aa8c38b2c321a6a4a79d6beef3ef798d4b08e9c2
FrederikRai <eaa24frra@students.eaaa.dk>
2026-05-20 12:51:50 +0200

    Fix JWT role claim mapping

--- Community/Program.cs
+++ Community/Program.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using System.Text;
 using Asp.Versioning;
 using Community.Repository;
@@ -16,21 +17,17 @@ try
 {
     var builder = WebApplication.CreateBuilder(args);
 
-    // Logging
     builder.Logging.ClearProviders();
     builder.Host.UseNLog();
 
-    // Controllers + Razor Pages + Swagger
     builder.Services.AddControllers();
     builder.Services.AddRazorPages();
     builder.Services.AddHttpClient();
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
 
-    // Cache
     builder.Services.AddMemoryCache();
 
-    // API Versioning
     builder.Services.AddApiVersioning(options =>
     {
         options.DefaultApiVersion = new ApiVersion(1, 0);
@@ -38,7 +35,6 @@ try
         options.ReportApiVersions = true;
     });
 
-    // JWT Authentication
     builder.Services
         .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         .AddJwtBearer(options =>
@@ -54,44 +50,37 @@ try
                 ValidAudience = builder.Configuration["Jwt:Audience"],
 
                 IssuerSigningKey = new SymmetricSecurityKey(
-                    Encoding.UTF8.GetBytes(
-                        builder.Configuration["Jwt:Secret"]!))
+                    Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"]!)),
+
+                RoleClaimType = ClaimTypes.Role,
+                NameClaimType = ClaimTypes.Name
             };
         });
 
-    // Authorization
     builder.Services.AddAuthorization();
 
-    // MongoDB
     builder.Services.AddSingleton<IMongoClient>(_ =>
-        new MongoClient(
-            builder.Configuration["Mongo:ConnectionString"]));
+        new MongoClient(builder.Configuration["Mongo:ConnectionString"]));
 
     builder.Services.AddScoped<IMongoDatabase>(provider =>
     {
         var client = provider.GetRequiredService<IMongoClient>();
-
-        return client.GetDatabase(
-            builder.Configuration["Mongo:DatabaseName"]);
+        return client.GetDatabase(builder.Configuration["Mongo:DatabaseName"]);
     });
 
-    // Dependency Injection
     builder.Services.AddScoped<ICommunityRepository, CommunityRepository>();
     builder.Services.AddScoped<ICommunityService, CommunityService>();
 
     var app = builder.Build();
 
-    // Swagger
     app.UseSwagger();
     app.UseSwaggerUI();
 
     app.UseHttpsRedirection();
 
-    // Authentication + Authorization
     app.UseAuthentication();
     app.UseAuthorization();
 
-    // Routes
     app.MapControllers();
     app.MapRazorPages();
 
